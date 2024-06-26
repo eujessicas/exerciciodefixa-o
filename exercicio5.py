278c2d2f511e3504a1d1d643bbefc86813772187
JESSICA AMANCIO <143044271+eujessicas@users.noreply.github.com>
2024-06-26 11:56:04 -0300

    Add files via upload

---
+++ exercicio5.py
@@ -0,0 +1,16 @@
+idade = 62
+if (idade > 60):
+    print("Você tem direito aos beneficios!")
+
+dano = 13
+escudo = 0
+if (dano > 10  and escudo == 0 ):
+    print("Você está morto!")
+
+norte = True
+sul   = False
+leste = False
+oeste = False
+
+if (norte == True or  sul == True or leste == True or oeste == True ):
+    print("Você escapou!")
